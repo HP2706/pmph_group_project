6fa51659bdcdb3d718e87b746a8ae392db76ffd6
ncg106 <ncg106@alumni.ku.dk>
2024-11-03 00:20:10 +0100

    Made cub work correctly and ensured it validates with our implementation!

--- src/cuda/tests/test_radix_sort_ker.cu
+++ src/cuda/tests/test_radix_sort_ker.cu
@@ -41,6 +41,55 @@ void quickvalidatesortedarray(
     }
 }
 
+template<class T>
+void quickvalidatecub(
+    uint32_t N,
+    T* arr_inp
+) {
+    bool success = true;
+    for (uint32_t i = 0; i < N-1; ++i)
+    {
+        if (arr_inp[i] > arr_inp[i+1])
+        {
+            printf("Element %d: %d is larger than element %d: %d in our CUB array, thus it is not sorted correctly.\n", i, arr_inp[i], i+1, arr_inp[i+1]);
+            success = false;
+        }
+    }
+    if (success)
+    {
+        printf("Sorted entire array successfully with CUB! (No errors)\n");
+    }
+    else
+    {
+        printf("Sorted entire array incorrectly with cub! (Errors)\n");
+    }
+}
+
+template<class T>
+void assertCubIsEqualToOurImplementation(
+    uint32_t N,
+    T* arr_inp,
+    T* arr_inpOurs
+) {
+    bool success = true;
+    for (uint32_t i = 0; i < N-1; ++i)
+    {
+        if (arr_inp[i] != arr_inpOurs[i])
+        {
+            printf("Element %d: %d from ours implementation is not equal to %d in our CUB array, thus it is not sorted correctly.\n", i, arr_inpOurs[i], arr_inp[i]);
+            success = false;
+        }
+    }
+    if (success)
+    {
+        printf("CUB sorted array matches our sorted array!\n");
+    }
+    else
+    {
+        printf("CUB sorted array does NOT match our sorted array!\n");
+    }
+}
+
 template<typename P>
 __host__ void test_radix_sort_ker(
     uint32_t input_size
@@ -140,21 +189,27 @@ __host__ void test_radix_sort_ker(
     printf("writing cub sort output to file\n");
 
 
-    /*for (int i = 0; i < input_size; i++) {
-        printf("cub_h_in[%d]: %u\n", i, cub_h_in[i]);
-    }*/
+
     // Calculate grid size based on input size
     int elements_per_block = P::BLOCK_SIZE * P::Q;
     int grid_size = (input_size + elements_per_block - 1) / elements_per_block;
     
-    CUBSortKernel<typename P::ElementType, P::BLOCK_SIZE, P::Q>
+    void* mem = NULL;
+    size_t len = 0;
+    /*CUBSortKernel<typename P::ElementType, P::BLOCK_SIZE, P::Q>
     <<<grid_size, P::BLOCK_SIZE>>>
     (
         cub_d_in, 
         cub_d_out, 
         input_size
-    );
+    );*/
+    //Get mem and length
     
+    uint32_t startBit = 0;
+    uint32_t endBit = 32;
+    
+    cub::DeviceRadixSort::SortKeys(mem, len, cub_d_in, cub_d_out, input_size, startBit, endBit);
+    cudaMalloc(&mem, len);
 
     cudaDeviceSynchronize();
     cudaError_t cub_err = cudaGetLastError();
@@ -162,9 +217,21 @@ __host__ void test_radix_sort_ker(
         printf("cub sort kernel failed: %s\n", cudaGetErrorString(cub_err));
         return;
     }
+
+    cub::DeviceRadixSort::SortKeys(mem, len, cub_d_in, cub_d_out, input_size, startBit, endBit);
+    cudaDeviceSynchronize();
+    
+    cub_err = cudaGetLastError();
+    if (cub_err != cudaSuccess) {
+        printf("cub sort kernel failed: %s\n", cudaGetErrorString(cub_err));
+        return;
+    }
+    
+    
     // copy results back to host
     cudaMemcpy(cub_h_out, cub_d_out, input_size * sizeof(typename P::ElementType), cudaMemcpyDeviceToHost);
-    
+    quickvalidatecub<uint32_t>(input_size, cub_h_out);
+    assertCubIsEqualToOurImplementation<uint32_t>(input_size, h_out, cub_h_out);
     
     std::remove("cub_sort_output.txt");
 
